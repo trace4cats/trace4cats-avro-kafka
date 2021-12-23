4fb8666d3c895b4f21c0167870ce6a3a521236f3
Scala Steward <me@scala-steward.org>
2021-12-23 18:23:46 +0100

    Update logback-classic to 1.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val fs2Kafka = "2.2.0"
     val kafka = "2.8.1"
     val log4cats = "2.1.1"
-    val logback = "1.2.9"
+    val logback = "1.2.10"
 
     val kindProjector = "0.13.2"
     val betterMonadicFor = "0.3.1"
